7d91a3a7fcf777fc9c30d9e9bbe13ac1ac475b61
Hanina Zahra <haninanz@gmail.com>
2025-06-25 14:40:08 +0700

    Update Readme.md

--- obj/Debug/net9.0/CSPractice.AssemblyInfo.cs
+++ obj/Debug/net9.0/CSPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa9cabd8b89f7127fdd0257336402072f5fbe2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18f1eb336bc25177bd8c8965b6cde6e6e56bb21")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
